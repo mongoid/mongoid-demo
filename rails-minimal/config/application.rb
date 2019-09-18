43938ac7ec278135bca43d39b126270375c7bbb6
Alex Bevilacqua <alex@alexbevi.com>
2019-09-18 10:32:39 -0400

    Renamed blog to rails-minimal

--- rails-minimal/config/application.rb
+++ rails-minimal/config/application.rb
@@ -5,8 +5,11 @@ require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 # require "active_record/railtie"
+# require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
@@ -19,11 +22,12 @@ Bundler.require(*Rails.groups)
 module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
     # Don't generate system test files.
     config.generators.system_tests = nil
